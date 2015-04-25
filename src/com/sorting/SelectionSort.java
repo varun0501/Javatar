f7cdcaba3ce6d32cf1d193c04d2faad1eb02a093
Varun Agarwal <varun0501@outlook.com>
2015-04-24 19:58:14 -0700

    Initial Commit

---
+++ src/com/sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+
+/*----------------------------------------------------------------
+ *  Author:        Varun Agarwal
+ *  Written:       3/21/2015
+ *  Last updated:  3/21/2015
+ *
+ *  Compilation:   javac SelectionSort.java
+ *  Execution:     java SelectionSort
+ *  
+ *  This program is to perform Selection Sort on the ten numbers
+ *  input by user
+ *  Input: ten numbers from user
+ *  Output: Sorted numbers using Selection Sort
+ *----------------------------------------------------------------*/
+package com.sorting;
+
+public class SelectionSort
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
